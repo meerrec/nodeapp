11ff90b65e1ef06fe9fd277dc2f7431e62029c04
Akash Paul <iakashpaul@gmail.com>
2017-06-28 13:17:08 +0530

    jQuery fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,33 @@
 (function() {
-	var socket = io.connect('http://45.76.150.25:8080');
-	
-	// Cache DOM selections
-	var $count = document.querySelector('.count'),
-		$theBtn = document.querySelector('.the-btn'),
-		$downloadBtn = document.querySelector('.download-btn');
-    $downloadBtn.hide();
-	// When the 'new-client-connection' event happens, set the count on the page
-	socket.on('new-client-connection', function(data) {
-		$count.innerHTML = numberWithCommas(data.count);
-	});
+    var socket = io.connect('http://45.76.150.25:8080');
 
-	// When the button is clicked, emit the 'btn-clicked' event
-	$theBtn.addEventListener('click', function() {
-		socket.emit('btn-clicked');
-	}, false);
+    // Cache DOM selections
+    var $count = document.querySelector('.count'),
+        $theBtn = document.querySelector('.the-btn'),
+        $downloadBtn = document.querySelector('.download-btn');
+    $("#download-btn").hide();
+    // When the 'new-client-connection' event happens, set the count on the page
+    socket.on('new-client-connection', function(data) {
+        $count.innerHTML = numberWithCommas(data.count);
+    });
 
-	// When the 'count-updated' event happens, update the count on the page
-	socket.on('count-updated', function(data) {
-		$count.innerHTML = numberWithCommas(data.count);
-	});
-	socket.on('file-ready', function(data) {
-		$downloadBtn.show();
-	});
-	$downloadBtn.addEventListener('click', function() {
-		socket.emit('download-clicked');
-	}, false);
+    // When the button is clicked, emit the 'btn-clicked' event
+    $theBtn.addEventListener('click', function() {
+        socket.emit('btn-clicked');
+    }, false);
 
-	function numberWithCommas(x) {
-		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	}
+    // When the 'count-updated' event happens, update the count on the page
+    socket.on('count-updated', function(data) {
+        $count.innerHTML = numberWithCommas(data.count);
+    });
+    socket.on('file-ready', function(data) {
+        $("#download-btn").show();
+    });
+    $downloadBtn.addEventListener('click', function() {
+        socket.emit('download-clicked');
+    }, false);
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 })();
