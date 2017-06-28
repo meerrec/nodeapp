739f3cdda3103f105a12cb5a6e2683fff5db8189
Akash Paul <iakashpaul@gmail.com>
2017-06-28 13:41:21 +0530

    WS

--- index.js
+++ index.js
@@ -16,74 +16,81 @@ app.use(fileUpload());
 
 // Express routes
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/upload',function(req,res){
- if (!req.files)
-    return res.status(400).send('No files were uploaded.');
- 
- let sampleFile = req.files.foo;
- 
-  // Use the mv() method to place the file somewhere on your server 
-  sampleFile.mv('/var/www/inputs/'+req.files.foo.name, function(err) {
-    if (err)
-      return res.status(500).send(err);
- 
-    res.send('File '+ req.files.foo.name + ' uploaded & saved!');
-		socket.emit('file-ready');
-  });
+app.post('/upload', function(req, res) {
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+
+    let sampleFile = req.files.foo;
+
+    // Use the mv() method to place the file somewhere on your server 
+    sampleFile.mv('/var/www/inputs/' + req.files.foo.name, function(err) {
+        if (err)
+            return res.status(500).send(err);
+
+        //res.send('File '+ req.files.foo.name + ' uploaded & saved!');
+        // socket.emit('file-ready');
+        Report();
+    });
 
 });
 app.get('/hello', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 
-// sbgui call
-	var child_process = require("child_process");
-    child_process.exec("./sbgui -scriptpath /var/www/temp/smartbody-cli-mod -script python7.py",{cwd:"/var/www/smartbody/bin"}, function(err, stdout, stderr) {
-      if (err) {
-        console.log(err.toString());
-      } else if (stdout !== "") {
-        console.log(stdout);
-				console.log("Finished execution")
-				// Send client a websocket message about the file being ready.
+    // sbgui call
+    var child_process = require("child_process");
+    child_process.exec("./sbgui -scriptpath /var/www/temp/smartbody-cli-mod -script python7.py", { cwd: "/var/www/smartbody/bin" }, function(err, stdout, stderr) {
+        if (err) {
+            console.log(err.toString());
+        } else if (stdout !== "") {
+            console.log(stdout);
+            console.log("Finished execution")
+                // Send client a websocket message about the file being ready.
 
-      } else {
-        console.log(stderr);
-      }
-   });
+        } else {
+            console.log(stderr);
+        }
+    });
 });
 
 // Socket.io
 io.on('connection', function(socket) {
-	var count;
+    var count;
 
-	// Read the count value from count.txt
-	fs.readFile('count.txt', 'utf-8', function(err, data) {
-		count = data;
+    // Read the count value from count.txt
+    fs.readFile('count.txt', 'utf-8', function(err, data) {
+        count = data;
 
-		// Send the count to the client when they initially connect
-		socket.emit('new-client-connection', {
-			count: data
-		});
-	});
+        // Send the count to the client when they initially connect
+        socket.emit('new-client-connection', {
+            count: data
+        });
+    });
 
-	// When a client clicks the button
-	socket.on('btn-clicked', function() {
+    // When a client clicks the button
+    socket.on('btn-clicked', function() {
 
-		// Read the count from count.txt
-		fs.readFile('count.txt', 'utf-8', function(err, data) {
-			count = data;
-			count ++;
+        // Read the count from count.txt
+        fs.readFile('count.txt', 'utf-8', function(err, data) {
+            count = data;
+            count++;
 
-			// Write the incremented value to count.txt
-			fs.writeFile('count.txt', count, function(err, data) {
+            // Write the incremented value to count.txt
+            fs.writeFile('count.txt', count, function(err, data) {
 
-				// Emit the 'count-updated' event to all connected clients with the updated count value
-				io.sockets.emit('count-updated', {
-					count: count
-				});
-			});
-		});
-	});
+                // Emit the 'count-updated' event to all connected clients with the updated count value
+                io.sockets.emit('count-updated', {
+                    count: count
+                });
+            });
+        });
+    });
+
+    function Report(socket) {
+        this.update = function(data) {
+            socket.emit('file-ready');
+        }
+    }
 });
