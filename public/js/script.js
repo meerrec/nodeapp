6566bfdff67ae85f9af884d09e8b1be915789844
Akash Paul <iakashpaul@gmail.com>
2017-06-28 17:48:33 +0530

    final rm click code

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,7 @@
     var socket = io.connect('http://45.76.150.25:8080');
 
     // Cache DOM selections
-    var $count = document.querySelector('.count'),
-        $theBtn = document.querySelector('.the-btn'),
-        $downloadBtn = document.querySelector('.download-btn');
+    var $downloadBtn = document.querySelector('.download-btn');
     $downloadBtn.style.display = "none";
 
 
@@ -12,20 +10,6 @@
         document.getElementById("uploadForm").submit();
     };
 
-    // When the 'new-client-connection' event happens, set the count on the page
-    socket.on('new-client-connection', function(data) {
-        $count.innerHTML = numberWithCommas(data.count);
-    });
-
-    // When the button is clicked, emit the 'btn-clicked' event
-    $theBtn.addEventListener('click', function() {
-        socket.emit('btn-clicked');
-    }, false);
-
-    // When the 'count-updated' event happens, update the count on the page
-    socket.on('count-updated', function(data) {
-        $count.innerHTML = numberWithCommas(data.count);
-    });
     socket.on('file-ready', function(data) {
         $downloadBtn.style.display = "";
     });
@@ -33,7 +17,4 @@
         socket.emit('download-clicked');
     }, false);
 
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
 })();
