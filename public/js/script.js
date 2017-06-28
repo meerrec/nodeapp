82b215d4688445cbeff2d23a2825538a86db8f00
Akash Paul <iakashpaul@gmail.com>
2017-06-28 13:08:55 +0530

    download button

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,11 @@
 (function() {
 	var socket = io.connect('http://45.76.150.25:8080');
-
+	
 	// Cache DOM selections
 	var $count = document.querySelector('.count'),
-		$theBtn = document.querySelector('.the-btn');
-
+		$theBtn = document.querySelector('.the-btn'),
+		$downloadBtn = document.querySelector('.download-btn');
+    $downloadBtn.hide();
 	// When the 'new-client-connection' event happens, set the count on the page
 	socket.on('new-client-connection', function(data) {
 		$count.innerHTML = numberWithCommas(data.count);
@@ -19,6 +20,12 @@
 	socket.on('count-updated', function(data) {
 		$count.innerHTML = numberWithCommas(data.count);
 	});
+	socket.on('file-ready', function(data) {
+		$downloadBtn.show();
+	});
+	$downloadBtn.addEventListener('click', function() {
+		socket.emit('download-clicked');
+	}, false);
 
 	function numberWithCommas(x) {
 		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
